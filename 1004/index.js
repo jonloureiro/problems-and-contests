e06bc6f0d4f784334e2110580a2de630df355b0f
Jonathan Loureiro <me@jonloureiro.dev>
2020-03-17 05:51:00 -0400

    1004

---
+++ 1004/index.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const a = parseInt(lines[0]);
+const b = parseInt(lines[1]);
+
+console.log("PROD =", a * b);
